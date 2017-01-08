e5b2ed0e016186b3a32bc9fc19e37d7fa71e4825
Cristian Buffa <bufface@gmail.com>
2017-01-08 17:14:28 -0300

    11. Component Properties

--- HelloReact/public/app.jsx
+++ HelloReact/public/app.jsx
@@ -1,15 +1,27 @@
 var Greeter = React.createClass({
+  getDefaultProps: function () {
+    return {
+      name: 'React',
+      message: 'This is from a componenet!'
+    };  
+  },
+
   render: function () {
+    var name = this.props.name;
+    var message = this.props.message;
+
     return (
       <div>
-        <h1>Hello React!</h1>
-        <p>This is from a componenet!</p>
+        <h1>Hello {name}!</h1>
+        <p>{message + '!!'}</p>
       </div>
     );
   }
 });
 
+var firstName = 'Cristian E';
+
 ReactDOM.render(
-  <Greeter/>,
+  <Greeter name={firstName} message="This is my message" />,
   document.getElementById('app')
 );
