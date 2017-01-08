88835775c616c760c112a86a7b3835257a87b843
Cristian Buffa <bufface@gmail.com>
2017-01-08 16:23:38 -0300

    9. Your First React Componenet

--- HelloReact/public/app.jsx
+++ HelloReact/public/app.jsx
@@ -1,4 +1,15 @@
+var Greeter = React.createClass({
+  render: function () {
+    return (
+      <div>
+        <h1>Hello React!</h1>
+        <p>This is from a componenet!</p>
+      </div>
+    );
+  }
+});
+
 ReactDOM.render(
-    <h1>Hello React!</h1>,
-    document.getElementById('app')
-  );
+  <Greeter/>,
+  document.getElementById('app')
+);
