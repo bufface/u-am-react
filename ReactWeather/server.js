7121e37dffd5a42a3a2a648f7ae1069007c94fc2
Cristian Buffa <bufface@gmail.com>
2017-01-10 16:18:52 -0300

    48. Fixing localhost

--- ReactWeather/server.js
+++ ReactWeather/server.js
@@ -5,11 +5,11 @@ var app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(function (req, res, next) {
-   if (req.headers['x-forwarded-proto'] === 'http') {
-     next();
-   } else {
-     res.redirect('http://' + req.hostname + req.url);
-   }
+  if (req.headers['x-forwarded-proto'] === 'http') {
+    res.redirect('http://' + req.hostname + req.url);
+  } else {
+    next();
+  }
 });
 
 app.use(express.static('public'));
