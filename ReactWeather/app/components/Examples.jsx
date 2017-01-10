a222ef6683d7ea23bfef750264f163d653705b26
Cristian Buffa <bufface@gmail.com>
2017-01-10 19:24:53 -0300

    53. Styling Examples Page

--- ReactWeather/app/components/Examples.jsx
+++ ReactWeather/app/components/Examples.jsx
@@ -1,10 +1,20 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 var Examples = (props) => {
   return (
     <div>
-      <h3>Examples</h3>
-      <p>Welcome to the examples page!</p>
+      <h1 className="text-center">Examples</h1>
+      <p>Here are a few examples locations to try out:</p>
+
+      <ol>
+        <li>
+          <Link to="/?location=Mendoza">Mendoza, ARG</Link>  
+        </li>
+        <li>
+          <Link to="/?location=BuenosAires">Buenos Aires, ARG</Link>  
+        </li>
+      </ol>
     </div>
   );
 };
