0b9f39afa08d0b66608ca7cc246ec1a1363ec98c
Cristian Buffa <bufface@gmail.com>
2017-01-10 11:53:50 -0300

    34. Making Our API Call

---
+++ ReactWeather/app/api/openWeatherMap.jsx
@@ -0,0 +1,20 @@
+var axios = require('axios');
+
+const OPEN_WEATHER_MAP_URL = 'http://api.openweathermap.org/data/2.5/weather?appid=a351582c9aef4a6936ad1c3778a3607d&units=metric';
+
+module.exports = {
+  getTemp: function (location) {
+    var encodedLocation = encodeURIComponent(location);
+    var requestURL = `${OPEN_WEATHER_MAP_URL}&q=${encodedLocation}`;
+
+    return axios.get(requestURL).then(function (res) {
+      if (res.data.code && res.data.message) {
+        throw new Error(res.data.message);
+      } else {
+        return res.data.main.temp;
+      }
+    }, function (res) {
+      throw new Error(res.data.message);
+    });
+  }
+}
