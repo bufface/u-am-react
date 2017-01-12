0660ca82565387d191036e028e9cd9a3026215e6
Cristian Buffa <bufface@gmail.com>
2017-01-12 15:52:52 -0300

    54. Styling About Page

--- ReactWeather/app/components/About.jsx
+++ ReactWeather/app/components/About.jsx
@@ -3,8 +3,9 @@ var React = require('react');
 var About = (props) => {
   return (
     <div>
-      <h3>About</h3>
-      <p>Welcome to the about page!</p>
+      <h1 className="text-center">About</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla adipisci accusamus, ipsum voluptatum, incidunt saepe earum aliquam fugit architecto rerum totam. Est quis tenetur excepturi corrupti! Optio rem corporis perferendis.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo enim ullam, aut beatae placeat nemo reprehenderit laudantium officia cum, libero temporibus maiores, repudiandae, cumque? Atque excepturi maiores ullam ipsam, magnam.</p>
     </div>
   );
 };
