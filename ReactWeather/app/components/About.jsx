67bed68d1ead01cb625b8643be3c8c948beb81e2
Cristian Buffa <bufface@gmail.com>
2017-01-12 17:35:34 -0300

    58. Adding Custom CSS

--- ReactWeather/app/components/About.jsx
+++ ReactWeather/app/components/About.jsx
@@ -3,7 +3,7 @@ var React = require('react');
 var About = (props) => {
   return (
     <div>
-      <h1 className="text-center">About</h1>
+      <h1 className="page-title text-center">About</h1>
       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla adipisci accusamus, ipsum voluptatum, incidunt saepe earum aliquam fugit architecto rerum totam. Est quis tenetur excepturi corrupti! Optio rem corporis perferendis.</p>
       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo enim ullam, aut beatae placeat nemo reprehenderit laudantium officia cum, libero temporibus maiores, repudiandae, cumque? Atque excepturi maiores ullam ipsam, magnam.</p>
     </div>
