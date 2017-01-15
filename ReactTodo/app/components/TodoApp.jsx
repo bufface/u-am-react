60e2b0965f35cb6209a17825ccf1e3e69b4400da
Cristian Buffa <bufface@gmail.com>
2017-01-15 16:14:23 -0300

    87. Rendering List In React (TodoList & Todo)

--- ReactTodo/app/components/TodoApp.jsx
+++ ReactTodo/app/components/TodoApp.jsx
@@ -1,10 +1,32 @@
 var React = require('react');
+var TodoList = require('TodoList');
 
 var TodoApp = React.createClass({
+  getInitialState: function () {
+    return {
+      todos: [
+        {
+          id: 1,
+          text: 'Walk the Dog'
+        }, {
+          id: 2,
+          text: 'Clean the yard'
+        }, {
+          id: 3,
+          text: 'Leave mail on porche'
+        }, {
+          id: 4,
+          text: 'Play video games'
+        }
+      ]
+    };
+  },
   render: function () {
+    var {todos} = this.state;
+
     return (
       <div>
-        TodoApp.jsx  
+        <TodoList todos={todos}/>
       </div>
     );
   }
