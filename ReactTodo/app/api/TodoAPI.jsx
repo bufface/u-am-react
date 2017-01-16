b2ad7bd230334f7fc29face487877e66a92bb07a
Cristian Buffa <bufface@gmail.com>
2017-01-16 19:57:50 -0300

    99. Wiring Up Search (Part 2)

--- ReactTodo/app/api/TodoAPI.jsx
+++ ReactTodo/app/api/TodoAPI.jsx
@@ -18,16 +18,28 @@ module.exports = {
     return $.isArray(todos) ? todos : [];
   },
   filterTodos: function (todos, showCompleted, searchText) {
-    var filterTodos = todos;
+    var filteredTodos = todos;
 
     // Filter by showCompleted
-    filterTodos = filterTodos.filter((todo) => {
+    filteredTodos = filteredTodos.filter((todo) => {
       return !todo.completed || showCompleted;
     });
     // Filter by searchText
-
+    filteredTodos = filteredTodos.filter((todo) => {
+      var text = todo.text.toLowerCase();
+      return searchText.length == 0 || text.indexOf(searchText) > -1;
+    });
     // Sort todos by non-completed first
+    filteredTodos.sort((a, b) => {
+      if (!a.completed && b.completed) {
+        return -1;
+      } else if (a.completed && !b.completed) {
+        return 1;
+      } else {
+        return 0;
+      }
+    });
 
-    return filterTodos;
+    return filteredTodos;
   }
 };
--- ReactTodo/app/tests/api/TodoAPI.test.jsx
+++ ReactTodo/app/tests/api/TodoAPI.test.jsx
@@ -52,7 +52,7 @@ describe('TodoAPI', () => {
       completed: true
     }, {
       id: 2,
-      text: 'Some text here',
+      text: 'Other text here',
       completed: false
     }, {
       id: 3,
@@ -67,6 +67,18 @@ describe('TodoAPI', () => {
     it('should return non-completed todos when showCompleted is false', () => {
       var filterTodos = TodoAPI.filterTodos(todos, false, '');
       expect(filterTodos.length).toBe(1);
+    });
+    it('should sort by completed status', () => {
+      var filteredTodos = TodoAPI.filterTodos(todos, true, '');
+      expect(filteredTodos[0].completed).toBe(false);
+    });
+    it('should filter todos by searchText', () => {
+      var filteredTodos = TodoAPI.filterTodos(todos, true, 'some');
+      expect(filteredTodos.length).toBe(2);
+    });
+    it('should return all todos if searchText is empty', () => {
+      var filteredTodos = TodoAPI.filterTodos(todos, true, '');
+      expect(filteredTodos.length).toBe(3);
     });
   });
 });
