595a28c140a977bbcec072d5282a0dda269f5a82
Cristian Buffa <bufface@gmail.com>
2017-01-15 22:24:38 -0300

    93. Wiring Up AddTodo

--- ReactTodo/app/tests/components/TodoApp.test.jsx
+++ ReactTodo/app/tests/components/TodoApp.test.jsx
@@ -9,4 +9,14 @@ describe('TodoApp', () => {
   it('should exist', () => {
     expect(TodoApp).toExist();
   });
+
+  describe('handleAddTodo', () => {
+    it('should add todo to todos state on handleAddTodo', () => {
+      var todoText = 'Test text';
+      var todoApp = TestUtils.renderIntoDocument(<TodoApp />);
+      todoApp.setState({ todos: [] });
+      todoApp.handleAddTodo(todoText);
+      expect(todoApp.state.todos[0].text).toBe(todoText);
+    });
+  });
 });
