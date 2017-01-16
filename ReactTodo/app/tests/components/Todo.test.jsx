f15458a1bc179d6656f876bb0656fb20ad5f9c50
Cristian Buffa <bufface@gmail.com>
2017-01-16 10:24:55 -0300

    96. Storing Todos In Local Storage

--- ReactTodo/app/tests/components/Todo.test.jsx
+++ ReactTodo/app/tests/components/Todo.test.jsx
@@ -20,5 +20,6 @@ describe('Todo', () => {
     var todo = TestUtils.renderIntoDocument(<Todo {...todoData} onToggle={spy} />);
     var $el = $(ReactDOM.findDOMNode(todo));
     TestUtils.Simulate.click($el[0]);
+    expect(spy).toHaveBeenCalledWith(199);
   });
 });
