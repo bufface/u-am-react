42509be80a7c1ac08ed5fb8cf242a7c51346d5d0
Cristian Buffa <bufface@gmail.com>
2017-02-05 16:34:31 -0300

    136. Testing Todo Updates

--- ReactTodo/app/tests/components/Todo.test.jsx
+++ ReactTodo/app/tests/components/Todo.test.jsx
@@ -3,7 +3,9 @@ var ReactDOM = require('react-dom');
 var expect = require('expect');
 var $ = require('Jquery');
 var TestUtils = require('react-addons-test-utils');
-var {Todo} = require('Todo');
+
+import * as actions from 'actions';
+import { Todo } from 'Todo';
 
 describe('Todo', () => {
   it('should exist', () => {
@@ -16,13 +18,11 @@ describe('Todo', () => {
       text: 'Some random text',
       completed: false
     };
+    var action = actions.startToggleTodo(todoData.id, !todoData.completed);
     var spy = expect.createSpy();
     var todo = TestUtils.renderIntoDocument(<Todo {...todoData} dispatch={spy} />);
     var $el = $(ReactDOM.findDOMNode(todo));
     TestUtils.Simulate.click($el[0]);
-    expect(spy).toHaveBeenCalledWith({
-      type: 'TOGGLE_TODO',
-      id: todoData.id
-    });
+    expect(spy).toHaveBeenCalledWith(action);
   });
 });
